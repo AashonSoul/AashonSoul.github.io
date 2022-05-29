7a52f38bde44b235fc2bc8836639728fbd047a50
AashonSoul <iloveyoumumma08@gmail.com>
2022-05-29 10:59:10 +0530

    Added new files

---
+++ script.js
@@ -0,0 +1,55 @@
+console.log("This is the combined project");
+alert("fix the period pug");
+alert("make it responsive");
+
+let string = "";
+let buttons = document.querySelectorAll('.input-button');
+
+//Printing the value by clicking the buttons
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        element = e.target
+        string = element.value;
+        let zero = document.getElementById('zero');
+        let period = document.getElementById('decimal')
+        let input = document.querySelector('#input').value
+        if(element == zero && input =='0'){
+            document.querySelector('#input').value != string;
+        }
+        else if(element == period && input=='.'){
+            document.querySelector('#input').value != string;
+        }
+        else{
+            document.querySelector('#input').value += string;
+        }
+    })
+});
+
+//Equal button for printing the output
+let equ = document.querySelector('#equal');
+equ.addEventListener('click', () => {
+    let input = document.querySelector('#input').value
+    let evaluated;
+    if(input.includes('.')){
+        evaluated = parseFloat(eval(input));
+        document.querySelector('#input').value = evaluated;
+    }
+    else {
+        evaluated = eval(input);
+        document.querySelector('#input').value = evaluated;
+    }
+});
+
+//All Clear button for clearing the output
+let ac = document.querySelector('#clear');
+ac.addEventListener('click', () => {
+    document.querySelector('#input').value = '';
+});
+
+//Delete button for Clearing one value from the input
+let del = document.querySelector('#erase');
+del.addEventListener('click', () => {
+    let input = document.querySelector('#input').value
+    sliced = (input.slice(0, -1))
+    document.querySelector('#input').value = sliced;
+});
